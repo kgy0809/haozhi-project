62e3107a832e55bfcda1e4febc5b286b0a7fdf70
zwj <zhangwenjun@bronet.cn>
2020-02-05 17:01:06 +0800

    2020.2.5

--- haozhi_greenroom/src/main/java/com/haozhi/greenroom/controller/HzYwController.java
+++ haozhi_greenroom/src/main/java/com/haozhi/greenroom/controller/HzYwController.java
@@ -1,17 +1,23 @@
 package com.haozhi.greenroom.controller;
 
 import com.github.pagehelper.PageInfo;
+import com.haozhi.common.constants.StatusCode;
 import com.haozhi.common.dto.PageResultDTO;
+import com.haozhi.common.dto.ResultDTO;
+import com.haozhi.common.utils.IdWorker;
 import com.haozhi.greenroom.pojo.Banner;
+import com.haozhi.greenroom.pojo.HzMenu;
 import com.haozhi.greenroom.pojo.HzYw;
 import com.haozhi.greenroom.service.HzYwService;
+import com.haozhi.greenroom.utils.ExcelUtil;
+import com.haozhi.greenroom.utils.FileUtil;
+import com.haozhi.greenroom.utils.QiniuUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 import java.util.Map;
 
@@ -26,34 +32,39 @@ public class HzYwController {
 
     @Autowired
     private HzYwService hzYwService;
+    @Autowired
+    private IdWorker idWorker;
 
     @RequestMapping
-    public String asd(){
+    public String asd() {
         return "tgls/system/brand/brand_list";
     }
 
-
-    @RequestMapping(value = "fy",method = RequestMethod.POST)
+    @RequestMapping(value = "fy", method = RequestMethod.POST)
     @ResponseBody
-    public PageResultDTO upload(@RequestParam(value = "page",defaultValue = "1")Integer page, @RequestParam(value = "rows",defaultValue = "10") Integer rows, Map<String, Object> map) {
+    public PageResultDTO upload(@RequestParam(value = "page", defaultValue = "1") Integer page, @RequestParam(value = "rows", defaultValue = "10") Integer rows, Map<String, Object> map) {
 
         PageInfo<HzYw> PageInfo = hzYwService.listImage(page, rows);
-        return new PageResultDTO(PageInfo.getTotal(),PageInfo.getList());
+        return new PageResultDTO(PageInfo.getTotal(), PageInfo.getList());
     }
 
-
-
-
     @RequestMapping("update")
-    public String queryById(String id,Map<String, Object> map){
+    public String queryById(String id, Map<String, Object> map) {
         HzYw hzYw = hzYwService.queryById(id);
-        map.put("hzYw",hzYw);
+        map.put("hzYw", hzYw);
         return "tgls/system/brand/brand_update";
     }
 
-    @RequestMapping(value = "update/add",method = RequestMethod.POST)
-    public String updateData(HzYw hzYw){
+    @RequestMapping(value = "update/add", method = RequestMethod.POST)
+    public String updateData(HzYw hzYw) {
         hzYwService.updateData(hzYw);
         return "redirect:/brand";
+    }
+
+    @RequestMapping("add/file")
+    @ResponseBody
+    public ResultDTO menu(MultipartFile file) throws Exception {
+        String upload = new QiniuUtil().upload(idWorker.nextId() + ""+".doc", file.getBytes());
+        return new ResultDTO(true, StatusCode.OK,"上传成功",upload);
     }
 }
